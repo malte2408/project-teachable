21189027e2466a6df33b5b99619fc9f15f398b80
malte2408 <maltemelzer@gmx.de>
2019-12-19 13:22:11 +0100

    add ui test

--- buttons.py
+++ buttons.py
@@ -26,4 +26,5 @@ class UI(object):
 ui = UI()
 
 while True: # Run forever
-    print(ui.getButtonState())
+    for index, button in enumerate(ui.getButtonState()):
+        ui.setLED(index, button)
