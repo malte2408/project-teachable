0ddcc11551de99287e62fb93b7fdd068b311c3dc
malte2408 <maltemelzer@gmx.de>
2020-01-05 16:46:17 +0100

    add ui test

--- buttons.py
+++ buttons.py
@@ -13,5 +13,5 @@ GPIO.setup(gpio, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 start = time.time()
 
 while time.time() - start < 20: # Run forever
-    if GPIO.input(gpio) == GPIO.HIGH:
-        print("Button was pushed!")
+    if GPIO.input(gpio) == GPIO.LOW:
+        print(GPIO.input(gpio))
