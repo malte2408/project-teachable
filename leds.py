d041875f2b3fc95de90c8ae1606da647c69e1a79
malte2408 <maltemelzer@gmx.de>
2020-01-05 17:01:00 +0100

    add ui test

---
+++ leds.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+
+gpio = int(sys.argv[1])
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(gpio, GPIO.OUT)
+
+GPIO.output(gpio, GPIO.LOW)
+
+time.sleep(2)
+
+GPIO.output(gpio, GPIO.HIGH)
